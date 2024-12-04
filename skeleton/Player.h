d594ad8d5fcc338abbedae1499fa0e5968019135
Chao <114423476+ChaoIsBalling@users.noreply.github.com>
2024-12-04 13:21:47 +0100

    me pregunto si esto cuenta como acabado

--- skeleton/Player.h
+++ skeleton/Player.h
@@ -4,7 +4,7 @@
 class Player:public RigidSolid
 {
 public:
-	Player(PxTransform p, Vector3 speed, float l, PxPhysics* g, PxScene* gS, Vector4 Color) :RigidSolid(p, speed, l, g, gS, Color,5,false) {};
+	Player(PxTransform p, Vector3 speed, float l, PxPhysics* g, PxScene* gS, Vector4 Color) :RigidSolid(p, speed, l, g, gS, Color,5,true) {};
 	void Move(Vector3 dir);
 	void Jump();
 };
